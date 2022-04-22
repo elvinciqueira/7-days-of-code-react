8fdab1f7685a35a0c1c18770b0c8c9ef4f6d99d5
Elvin Ciqueira <elvinciqueira@gmail.com>
2022-04-22 15:42:23 -0300

    refactor: handler pattern

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -2,20 +2,34 @@ import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const createMail = (to) => ({
-  to, // Change to your recipient
-  from: 'elvin.ciqueira@ferreri.co', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: "Olá. Boas-vindas à Casa Verde! Você se cadastrou em nossa newsletter e vai começar a receber e-mails com as novidades de nossa loja e dicas de como cuidar de suas plantas. Até logo!",
-});
+const controller = {
+  async sendMail(req, res) {
 
-export default async function useHandler(req, res) {
-  const { body: { email } } = req;  
-    
-  try {
-    const [response] = await sgMail.send(createMail(email));
-    res.status(response.statusCode).json({ success: true });
-  } catch (error) {
-    res.status(error.code).json({ error: error.message });
+    const createMail = (to) => ({
+      to, // Change to your recipient
+      from: 'elvin.ciqueira@ferreri.co', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: "Olá. Boas-vindas à Casa Verde! Você se cadastrou em nossa newsletter e vai começar a receber e-mails com as novidades de nossa loja e dicas de como cuidar de suas plantas. Até logo!",
+    });
+
+    try {
+      const [response] = await sgMail.send(createMail(req.body.email));
+      res.status(response.statusCode).json({ success: true });
+    } catch (error) {
+      res.status(error.code).json({ error: error.message });
+    }
   }
+}
+
+controllerBy = {
+  POST: controller.sendMail
+}
+
+export default async function handler(request, response) {
+  if (controllerBy[request.method]) return controllerBy[request.method](request, response);
+
+  response.status(404).json({
+    status: 404,
+    message: 'Not Found'
+  });
 }
